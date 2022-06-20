ae3d002793987890abbf0a39e8d33a04c58b7aff
Guillermo-Prieto <guiprirod@alum.us.es>
2022-06-20 18:49:03 +0200

    main

--- FRONT/Screens/Home.js
+++ FRONT/Screens/Home.js
@@ -1,91 +1,27 @@
 // import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
-import { event } from 'react-native-reanimated'
-import axios from 'axios'
-import { TextInput } from 'react-native-gesture-handler'
-import { TouchableOpacity } from 'react-native-web'
+// import { event } from 'react-native-reanimated'
+// import axios from 'axios'
+// import { TextInput } from 'react-native-gesture-handler'
+// import { TouchableOpacity } from 'react-native-web'
 
-export default function About ({ navigation }) {
+export default function Home ({ navigation }) {
   const goAbout = () => {
     navigation.navigate('About')
   }
-
-  //VIDEO DE LISTADO
-  constructor() {
-    super()
-    this.guardar= this.guardar.bind(this)
-    this.state={
-      lista:[],
-      nombre:'',
-      precio:'',
-    }
-  }
-  componentDidMount(){
-    this.getProducto();
-  }
-
-  async getProducto() {
-  try{
-    const res= await axios.get('localhost');
-    console.log(res.data)
-    this.setState({lista:res.data})
-
-  }catch (error) {console.error(error)}
-
-  async guardar(event){
-    event.preventDefault();
-    try{
-      let{nombre,precio}= this.state;
-      const obj= {nombre,precio}
-      const res = await axios.post('localhost',obj);
-      console.log(res.data)
-    }catch(error){ console.log(error)
-      this.setState({nombre:'', precio:''})
-      this.getProducto();
-
-    }
-
-  }
-// HASTA AQUI
   return (
     <View style={styles.container}>
       <Text>Bienvenido a APP  </Text>
 
       <Button style={styles.iniciar} title='Ir About' onPress={goAbout} backgroundColor='red'/>
 
-      <TextInput style={styles.input}
-      placeholder="Nombre"
-      onChangeText={(nombre)=>this.setState({nombre})}
-      value={this.state.nombre}/>
-
-<TextInput style={styles.input}
-      placeholder="Precio"
-      onChangeText={(precio)=>this.setState({precio})}
-      value={this.state.nombre}/>
-      
-     <TouchableOpacity style={styles.buttonGuardar}>
-      onPress={this.guardar}
-      <Text style={styles.ButtonTexto}>Guardar</Text>
-
-     </TouchableOpacity>
-
-     <View style={{flex:1}}>
-      {this.state.lista.map(item =>(
-        <View style={styles.mycard} key={item.id}>
-          <Text>{item.nombre}</Text>
-          <Text>{item.precio}</Text>
-        </View>
-      ))}
-     </View>
-
     </View>
   )
 }
-}
 
 const styles = StyleSheet.create({
-  container: { 
+  container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
@@ -94,27 +30,27 @@ const styles = StyleSheet.create({
     shadowRadius: 0,
     shadowColor: 'transparent'
   },
-  mycard:{
+  mycard: {
     backgroundColor: '#E8EBEC',
-    padding:20,
-    marginVertical:5,
-    marginHorizontal:12,
-    elevation:3
+    padding: 20,
+    marginVertical: 5,
+    marginHorizontal: 12,
+    elevation: 3
   },
-  buttonGuardar:{
-    backgroundColor:'#C132F3',
-    padding:12,
-    marginHorizontal:15,
-    marginTop:10,
-    marginBottom:12,
-    borderRadius:24,
+  buttonGuardar: {
+    backgroundColor: '#C132F3',
+    padding: 12,
+    marginHorizontal: 15,
+    marginTop: 10,
+    marginBottom: 12,
+    borderRadius: 24
 
-    },
-  ButtonTexto:{
-    alignSelf:'center',
-    color:'white',
-    fontSize:19,
-    fontWeight:'bold'
+  },
+  ButtonTexto: {
+    alignSelf: 'center',
+    color: 'white',
+    fontSize: 19,
+    fontWeight: 'bold'
   },
   input: {
     borderWidth: 1,
