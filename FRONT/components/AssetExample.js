d176320b14cd2a4ca2408f567cc764d25eaf5439
Paula Yuan César Aguilar <paucesagu@alum.us.es>
2022-06-20 12:18:16 +0200

    main2

--- FRONT/components/AssetExample.js
+++ FRONT/components/AssetExample.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, Image } from 'react-native';
 
-export default function AssetExample() {
+export default function AssetExample () {
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>
@@ -16,17 +16,17 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 24,
+    padding: 24
   },
   paragraph: {
     margin: 24,
     marginTop: 0,
     fontSize: 14,
     fontWeight: 'bold',
-    textAlign: 'center',
+    textAlign: 'center'
   },
   logo: {
     height: 128,
-    width: 128,
+    width: 128
   }
 });
